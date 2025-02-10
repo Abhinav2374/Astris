ecd01353ffafdf9ea99cbadc45a2f455cd95275a
Abhinav2374 <amctitan123@gmail.com>
2025-02-10 22:45:31 +0530

    added language monitoring feature 🚨

--- src/bot.js
+++ src/bot.js
@@ -1,54 +1,68 @@
-require('dotenv').config()
-const {Client,GatewayIntentBits} = require('discord.js');
-const axios = require('axios');
+require("dotenv").config();
+const { Client, GatewayIntentBits } = require("discord.js");
+const axios = require("axios");
 
 const client = new Client({
-    intents:[
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent
-    ]
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
 });
 
-function splitMessage(text, maxLength = 2000){
-    const chunks = [];
-    for(let i =0;i< text.length ;i += maxLength){
-        chunks.push(text.slice(i,i+maxLength));
-    }
-    return chunks;
+function splitMessage(text, maxLength = 2000) {
+  const chunks = [];
+  for (let i = 0; i < text.length; i += maxLength) {
+    chunks.push(text.slice(i, i + maxLength));
+  }
+  return chunks;
 }
 
+const bannedWords = ["nigg"];
 
-client.once('ready',()=>{
-    console.log(`logged in as ${client.user.tag}`);
-})
+client.once("ready", () => {
+  console.log(`logged in as ${client.user.tag}`);
+});
 
-client.on('messageCreate',async (message)=>{
-    if(message.author.bot) return;
-    const userInput = message.content.trim();
-    try {
-        const response = await axios.post(
-            "https://api.groq.com/openai/v1/chat/completions",
-            {
-                model: "llama3-8b-8192",
-                messages: [{role:"user",content: userInput }]
-            },
-            {
-                headers:{
-                    "Authorization": `Bearer ${process.env.GORQ_API}`,
-                    "Content-Type": 'application/json'
-                }
-            }
-        );
-        const botReply = response.data.choices?.[0]?.message?.content || 'Sorry but i couldnt generate an answer for this.';
-        const reply = splitMessage(botReply);
-        for (const replies of reply){
-            await message.reply(replies);
-        }
-    }catch(error){
-        console.error("Error with Groq api",error);
-        message.reply("Oops! something went wrong");
+client.on("messageCreate", async (message) => {
+  if (message.author.bot) return;
+  const userInput = message.content.trim();
+  try {
+    const response = await axios.post(
+      "https://api.groq.com/openai/v1/chat/completions",
+      {
+        model: "llama3-8b-8192",
+        messages: [{ role: "user", content: userInput }],
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.GORQ_API}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const botReply =
+      response.data.choices?.[0]?.message?.content ||
+      "Sorry but i couldnt generate an answer for this.";
+    const reply = splitMessage(botReply);
+    for (const replies of reply) {
+      await message.reply(replies);
     }
-})
+  } catch (error) {
+    console.error("Error with Groq api", error);
+    message.reply("Oops! something went wrong");
+  }
+
+  if (bannedWords.some((word) => msg.content.toLowerCase().includes(word))) {
+    try {
+      await msg.author.send(
+        `🚨 your msg was deleted from **#${msg.channel.name}** because it contained offensive words`
+      );
+      await msg.delete();
+    } catch (error) {
+      console.log("failed to delete message", error);
+    }
+  }
+});
 
 client.login(process.env.DISCORD_TOKEN);
